fc6bafdf8c1937395f91565c1eb6eee5d87c29a2
Mateusz Jamroz <jamr.mat@gmail.com>
2023-10-06 23:40:14 +0200

    initial commit

---
+++ src/main/java/com/webdev/cosmo/cosmobackend/CosmoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webdev.cosmo.cosmobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webdev/cosmo/cosmobackend/CosmoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.cosmo.cosmobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
