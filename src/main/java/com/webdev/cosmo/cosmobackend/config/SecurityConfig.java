a0488657d5b3cbd953f81c0bf6dbc41d388d8556
Jan Kołek <jankolek0@gmail.com>
2023-10-13 19:25:31 +0200

    initial oauth2 config

--- src/main/java/com/webdev/cosmo/cosmobackend/config/SecurityConfig.java
+++ src/main/java/com/webdev/cosmo/cosmobackend/config/SecurityConfig.java
@@ -9,15 +9,18 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/verification/unsecured").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults());
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults());
         return http.build();
     }
 }
---
+++ src/main/java/com/webdev/cosmo/cosmobackend/controller/VerificationController.java
@@ -0,0 +1,23 @@
+package com.webdev.cosmo.cosmobackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/verification")
+public class VerificationController {
+
+    @GetMapping("/unsecured")
+    public String getUnsecuredEndpoint(){
+        return "Unsecured";
+    }
+
+    @GetMapping("/secured")
+    public String getSecuredEndpoint(){
+        return "Secured content";
+    }
+
+
+
+}
