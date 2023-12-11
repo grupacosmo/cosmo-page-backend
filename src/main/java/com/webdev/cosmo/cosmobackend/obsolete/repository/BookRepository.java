1b2ee63d62f9f4eac13e06816b1b397e80c62d8c
Mateusz Jamróz <jamr.mat@gmail.com>
2023-12-11 11:34:59 +0100

    Configure cicd with test app deployment (#6)
    Configure cicd with test app deployment

--- src/main/java/com/webdev/cosmo/cosmobackend/obsolete/repository/BookRepository.java
+++ src/main/java/com/webdev/cosmo/cosmobackend/obsolete/repository/BookRepository.java
@@ -1,9 +1,10 @@
 package com.webdev.cosmo.cosmobackend.obsolete.repository;
 
 import com.webdev.cosmo.cosmobackend.obsolete.schemas.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends ReactiveCrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
