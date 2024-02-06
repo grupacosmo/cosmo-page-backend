ba7b823db81a81647cd3fd00693931f10ba998a6
Mateusz Jamroz <jamr.mat@gmail.com>
2024-02-06 12:48:08 +0100

    PR test

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,22 @@
 
 start=$(date +"%s")
 
-ssh -p 22 $2@$1 -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+ssh -p 22 $2@$1 -i key.txt -t -o StrictHostKeyChecking=no << ENDSSH
 docker pull cosmopk/cosmo-backend:latest
 
 CONTAINER_NAME=cosmo-backend
-if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
-    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+if [ "\$(docker ps -qa -f name=\$CONTAINER_NAME)" ]; then
+    if [ "\$(docker ps -q -f name=\$CONTAINER_NAME)" ]; then
         echo "Container is running -> stopping it..."
-        docker stop $CONTAINER_NAME;
+        docker stop \$CONTAINER_NAME;
     fi
 fi
 
-docker run -d --rm -p 8000:8000 --name $CONTAINER_NAME cosmopk/cosmo-backend:latest
+docker run -d --rm -p 8000:8000 --name \$CONTAINER_NAME cosmopk/cosmo-backend:latest
 
 exit
 ENDSSH
+
 
 if [ $? -eq 0 ]; then
   exit 0
