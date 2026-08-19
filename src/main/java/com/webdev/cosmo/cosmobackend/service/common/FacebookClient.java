cb7e21f457afbebc5edcd8652636095d81b93560
Zuzanna Zawartka <zawartka.zuzanna@gmail.com>
2026-08-19 14:48:00 +0200

    feat: emulate Facebook API for local development
    - add FacebookClientEmulator serving sample posts when
      facebook.emulator.enabled=true, so dev runs without a real token
    - FacebookClientConfig: conditional beans - emulator when enabled,
      real Feign client (via FeignClientFactoryBean) otherwise
    - drop @FeignClient/@EnableFeignClients in favour of the explicit bean
    - auto-seed sample posts at startup in emulator mode

--- src/main/java/com/webdev/cosmo/cosmobackend/service/common/FacebookClient.java
+++ src/main/java/com/webdev/cosmo/cosmobackend/service/common/FacebookClient.java
@@ -4,10 +4,8 @@ import org.openapitools.model.AccessTokenRs;
 import org.openapitools.model.FacebookResponse;
 import org.openapitools.model.FacebookUser;
 import org.openapitools.model.LongLivedAccessToken;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "facebookAccessTokenClient", url = "https://graph.facebook.com")
 public interface FacebookClient {
 
     @GetMapping(path = "/oauth/access_token")
